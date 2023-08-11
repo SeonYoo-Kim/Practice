a2811d32f7e85a04413f528f90312e9b29312848
theta <tjdn13@naver.com>
2023-08-11 17:15:47 +0900

    all

--- resize.py
+++ resize.py
@@ -2,17 +2,16 @@ from configparser import Interpolation
 import cv2
 from PIL import Image, ImageOps
 import os
-# 업스케일링
-LR_path = "C:/Users/tjdn9/Documents/srdata/T-91/LR_bicubic_PIL/X8"
+# 업스케일링 C:\Users\tjdn9\Documents\srdata\Set5\HR
+LR_path = "C:/Users/tjdn9/Documents/srdata/Set5/LR_bicubic_PIL/X4"
 filenames = [filename for filename in os.listdir(LR_path) if filename.endswith(".png")]
-
+HR_path = "C:/Users/tjdn9/Documents/srdata/Set5/HR"
 # 다운스케일링
-# HR_path = "C:/Users/tjdn9/Documents/srdata/T-91/HR"
+# HR_path = "C:/Users/tjdn9/Documents/srdata/Set5/HR"
 # filenames = [filename for filename in os.listdir(HR_path) if filename.endswith(".png")]
 
-save_path = "C:/Users/tjdn9/Documents/srdata/T-91/LR_bicubic_PIL/X8_resized"
-scale = 8
-
+save_path = "C:/Users/tjdn9/Documents/srdata/Set5/LR_bicubic_PIL/X4_resized"
+scale = 4
 
 def mkdir(path):
     if not os.path.exists(path):
@@ -37,12 +36,17 @@ def CV2_UP(filename, LR_path, save_path):
     resized_img = cv2.resize(LR, ((LR.shape[0] * scale), (LR.shape[1] * scale)), cv2.INTER_CUBIC)
     save_name = os.path.join(save_path, filename)
     cv2.imwrite(save_name, resized_img)
-
+def PIL_HRsize(filename, LR_path, HR_path, save_path):
+    LR = Image.open(os.path.join(LR_path, filename)).convert('RGB')
+    HR = Image.open(os.path.join(HR_path, filename)).convert('RGB')
+    resized = LR.resize((HR.size[0], HR.size[1]), Image.BICUBIC)
+    save_name = os.path.join(save_path, filename)
+    resized.save(save_name)
 if __name__ == "__main__":
     mkdir(save_path)
-
     for filename in filenames:
-        # PIL_DOWN(filename, HR_path, save_path)
-        PIL_UP(filename, LR_path, save_path)
-        # CV2_DOWN(filename, HR_path, save_path)
+        #PIL_DOWN(filename, HR_path, save_path)
+        #PIL_UP(filename, LR_path, save_path)
+        CV2_DOWN(filename, HR_path, save_path)
         #CV2_UP(filename, LR_path, save_path)
+        #PIL_HRsize(filename, LR_path, HR_path, save_path)
